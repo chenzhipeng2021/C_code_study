960af1a503ef338f389e2dd0bfb99458346b3381
chenzhipeng2021 <371750220@qq.com>
2021-11-07 15:41:43 +0800

    这是一个用wihle循环来练习文字关门显示效果的小练习

---
+++ while xunhuan1.c
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string.h>
+#include <windows.h>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	char stra[]="chen zhipeng 2021 hehe!";
+	char strb[]="#######################";
+	int left=0;
+	int right=strlen(stra)-1; //strlen()是求字符串长度，它用string.h头文件 
+	while(left<=right){
+		strb[left]=stra[left];
+		strb[right]=stra[right];
+		printf("%s\n",strb);
+		Sleep(1000);   //这是等待函数，它存在windows.h头文件中，注意这个函数的第一个字母要大写
+		system("cls");//这是清屏函数 
+	 	left++;
+	 	right--;		
+	}	
+	printf("%s\n",strb); 
+	return 0;
+	
+}
